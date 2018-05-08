6ba08c5ce59f318ad3a1974a27dbd13ee38611ad
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-05-08 16:28:22 +0300

    debugging

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -14,5 +14,5 @@ from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
-
+application = DjangoWhiteNoise(application)
 application = get_wsgi_application()
